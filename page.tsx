b002bb26143c85b3d008c187bf7ada739e1c0267
itsayii <153350557+itsayii@users.noreply.github.com>
2023-12-11 19:04:01 +0700

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,62 @@
+import { PrismaClient } from "@prisma/client";
+import AddProduct from "./addproduct";
+import DeleteProduct from "./deleteproduct";
+import UpdateProduct from "./updateproduct";
+const prisma = new PrismaClient();
+
+const getProduct = async () => {
+    const res = await prisma.product.findMany({
+        select: {
+            id: true;
+            title: true;
+            price: true;
+            brandId: true;
+            brand: true;
+        },
+    });
+    return res;
+};
+const getBrands = async () => {
+    const res = await prisma.brand.findMany();
+    return res;
+};
+   
+const Product = async () => {
+    const [products, brands] = await Promise.all([getProduct(), getBrands()]);
+
+    return (
+        <div>
+            <h1 className="text-center font-bold">Store kita</h1>
+            <div className="mb-2">
+                <AddProduct brands={brands} />
+            </div>
+            <table className="table w-full">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>ProductName</th>
+                        <th>Price</th>
+                        <th>Brand</th>
+                        <th className="text-center">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map((product, index) => (
+                        <tr key={product.id}>
+                            <td>{index + 1}</td>
+                            <td>{product.title}</td>
+                            <td>{product.price}</td>
+                            <td>{product.brand.name}</td>
+                            <td className="flex justify-center space-x-1">
+                                <UpdateProduct brands ={brands} product={product} />
+                                <DeleteProduct product={product} />
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default UpdateProduct;
